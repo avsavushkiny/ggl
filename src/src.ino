a5dc34f4d75b7f91fcd12cfb11df30c42de0e2f5
Alexander Savushkin <74372739+avsavushkiny@users.noreply.github.com>
2024-03-13 00:43:17 +0300

    Update src.ino

--- src/src.ino
+++ src/src.ino
@@ -12,16 +12,16 @@ void loop()
 {
 
   // monochrome
-  ggl.clearBuffer();
+  /*ggl.clearBuffer();
   ggl.drawXBMP(106, 58, 44, 44, ex_m);
   ggl.writeString(0, 10, "Experience", 12, 1);
   ggl.display();
-  delay(5000);
+  delay(5000);*/
 
   // gray
   ggl.clearBuffer();
-  ggl.bitmapGray(10, 10, toggle, 32, 32);
-  ggl.writeGrayString(0, 10, "Experience", 12, 1);
+  ggl.bitmapGray(5, 10, toggle, 32, 32);
+  ggl.writeGrayString(5, 50, "Experience", 12, 1);
   ggl.displayGray();
   delay(5000);
 }
